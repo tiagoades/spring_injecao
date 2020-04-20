7e3c8d54bce4b5bd3ab5ec42e30182c9e2700434
tiagoades <tiago.almeida.s@gmail.com>
2020-04-20 12:47:30 -0300

    Aulas 2.14 e 2.15

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,28 +1,46 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.model.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
+	@Autowired(required = false)
 	private Notificador notificador;
-	
-	public AtivacaoClienteService(Notificador notificador) {
-	
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: "+notificador);
-		
-	}
-	
+
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//	
+//		this.notificador = notificador;
+//						
+//	}
+
+//	public AtivacaoClienteService(String qualquerCoisa) {
+//		
+//	}
+
 	public void ativar(Cliente cliente) {
 
 		cliente.ativar();
 
-		this.notificador.notificar(cliente, "Seu cdastro no sistema esta ativo!");
-
+		if (notificador != null) {
+		
+			this.notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+		
+		} else {
+		
+			System.out.println("Não existe um notificador!!1");
+		
+		}
 	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
 
 }
